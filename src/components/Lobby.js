ec5826832293267d09403597f3e6086cce666d4c
jmonica9 <jmzmonica99@gmail.com>
2022-10-15 17:27:59 +0800

    new

--- src/components/Lobby.js
+++ src/components/Lobby.js
@@ -322,7 +322,7 @@ export default function Lobby(props) {
               sx={{
                 // width: props.drawerOpen ? "35vw" : "45vw",
                 backgroundColor: themeColor,
-                height: "45vh",
+                height: "35vh",
                 display: "flex",
                 borderRadius: 25,
               }}
@@ -348,7 +348,7 @@ export default function Lobby(props) {
               sx={{
                 // width: props.drawerOpen ? "35vw" : "45vw",
                 backgroundColor: themeColor,
-                height: "45vh",
+                height: "35vh",
                 display: "flex",
                 borderRadius: 25,
               }}
